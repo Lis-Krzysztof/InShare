1180279aa2eccf56728a3518f681b67147c59b15
Lis-Krzysztof <lis.krzysztof.kazimierz@gmail.com>
2021-05-22 00:41:14 +0200

    added login and logout data connections

--- website/models.py
+++ website/models.py
@@ -2,3 +2,27 @@ import pyodbc
 import base64
 
 
+query='SELECT * FROM dbo.Users'
+
+
+
+
+def readData(query):
+    server = '.' 
+    database = 'test1' 
+    username = 'user2' 
+    password = "A"
+    con = pyodbc.connect('DRIVER={SQL Server Native Client 11.0};SERVER='+server+'; DATABASE='+database+'; UID='+username+';PWD='+password, autocommit=True)
+    cur = con.cursor()
+    cur.execute(query)
+    data = cur.fetchall()
+    if not data:
+        cur.close()
+        return False
+    else:
+        for row in data:
+            row_to_list = [elem for elem in row]
+        cur.close()
+        return row_to_list
+    
+readData(query)
--- website/views.py
+++ website/views.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+from flask.globals import session
 
 views = Blueprint('views', __name__)
 
@@ -6,3 +7,16 @@ views = Blueprint('views', __name__)
 @views.route('/')
 def home():
     return render_template("home.html")
+
+@views.route('/search')
+def search():
+    return render_template("search.html", session=True)
+
+@views.route('/choose_inpost')
+def choose_inpost():
+    return render_template("choose_inpost.html", session=True)
+
+
+@views.route('/login')
+def login():
+    return render_template("login.html")
