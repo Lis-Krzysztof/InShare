b417d6448848061cef757f09575f4f083a5a777f
Lis-Krzysztof <lis.krzysztof.kazimierz@gmail.com>
2021-05-26 01:52:56 +0200

    Aded options to create edit delete offers

--- website/views.py
+++ website/views.py
@@ -1,16 +1,14 @@
 from flask import Blueprint, render_template
-<<<<<<< HEAD
 from .models import readData, readTable
-=======
+
 from flask.globals import session
->>>>>>> 676ac8badbf7b7f069fa29c94ee0f848a10dea7b
+
 
 views = Blueprint('views', __name__)
 
 
 @views.route('/')
 def home():
-<<<<<<< HEAD
     all_data = readTable('SELECT * FROM ActiveOffers')
     return render_template("home.html", data = all_data)
 
@@ -23,19 +21,12 @@ def search():
 def choose_inpost():
     all_data = readTable('SELECT * FROM Paczkomaty order by kod')
     return render_template("choose_inpost.html", session=True, data = all_data)
-=======
-    return render_template("home.html")
-
-@views.route('/search')
-def search():
-    return render_template("search.html", session=True)
-
-@views.route('/choose_inpost')
-def choose_inpost():
-    return render_template("choose_inpost.html", session=True)
->>>>>>> 676ac8badbf7b7f069fa29c94ee0f848a10dea7b
-
 
 @views.route('/login')
 def login():
     return render_template("login.html")
+
+@views.route('/my_offers')
+def my_offers():
+    all_data = readTable('SELECT * FROM MyOffers WHERE userId = '+ str(session['id']) )
+    return render_template('my_offers.html', session=True, data = all_data)
